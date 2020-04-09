326c698091e16d1ca4f0b68f6fdafc7fbf0b5535
ITTV1 <62187261+ITTV1@users.noreply.github.com>
2020-04-09 20:11:12 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+int fsclose(int $f)
+
+fopen()
+
+
+Запись и чтение файла
+
+Построчную запичь и чтение
+
+string fgets(int $a [, int $l]
+
+file()
+
+int fputs(int $a, string $s)
+
+?>
---
+++ csv.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+Чтение CSV файла
+
+
+   list     fgetcsv(
+       int $a,
+   [int $l = 0,]
+   [string $d = ',',]
+   [string $q = '"',]
+     [string $e = '\'] )
+
+
+?>
